410e1237aacfd103ca0aa7025fe959fbb2958d76
house <housesellout@gmail.com>
2023-05-29 22:14:56 -0400

    :- ** HOUSE initial commit **: added func gen_from_seed 	- added feature from CLI 		- takes in a custom seed phrase and generates an x number of 			addresses from it 				(as opposed to just generating a seed phrase 					and a single address from it)

--- ethereum/genethkey.py
+++ ethereum/genethkey.py
@@ -10,6 +10,32 @@ import qrcode
 
 listHoldAddress = []
 
+def gen_from_seed(language, entropyStrength, seedPhrase, account_num=0):
+    seedPhraseLength = len(seedPhrase.split(" "))
+    
+    # generate wallet w/ a single private/public key (address)
+    wallet = bip44.Wallet(seedPhrase)
+    secretKey, publicKey = wallet.derive_account("eth", account=account_num)
+    
+    # generate coincurve private key instance (to do stuff with...?)
+    secretKey = coincurve.PrivateKey(secretKey)
+    
+    # coincurve instance validation
+    if secretKey.public_key.format() != publicKey:
+        print("\n** WARNING **: secretKey derived from coincurve DOES NOT match publicKey dervived from bip44")
+    else:
+        print("\n... note: coincurve validation success!")
+        
+    # derive publc address from public key?
+    address = bip44.utils.get_eth_addr(publicKey)
+
+    listHoldAddress.append(address)
+    
+    # print everything
+    print("\nSeed Phrase ({} words):\n {}" .format(seedPhraseLength, seedPhrase))
+    print("\naddress: {}" .format(address))
+    print("secret: {}\n".format(secretKey.to_hex()))
+    
 def gen_eth_key(language, entropyStrength):
     # generate seed phrase
     mnemo = mnemonic.Mnemonic(language)
@@ -27,7 +53,7 @@ def gen_eth_key(language, entropyStrength):
     if secretKey.public_key.format() != publicKey:
         print("\n** WARNING **: secretKey derived from coincurve DOES NOT match publicKey dervived from bip44")
     else:
-        print("\n... note: coincurve validation checked out just fine")
+        print("\n... note: coincurve validation success!")
         
     # derive publc address from public key?
     address = bip44.utils.get_eth_addr(publicKey)
@@ -71,4 +97,12 @@ if __name__ == "__main__":
             execute = obtain_address_qr()
         if command == str("--help") or command == str("-h"):
             execute = help_me()
+        if command == str("--restore-test") or command == str("-rt"):
+            seed = "zero one two three four five six seven eight nine ten eleven thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty twenty-one twenty-two twenty-three"
+            if not len(seed):
+                print("\n** ERROR **: no seed phrase manually entered in code\n")
+            else:
+                for x in range(0,10):
+                    print(f"account # {x}")
+                    execute = gen_from_seed(language, entropyStrength, seed, x)
     print("DONE - genethkey.py")
